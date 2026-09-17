ab4b01a673bb814a3d07df9c876eb9e58f208cc6
luisleon <luis.escalante@upc.com.mx>
2026-09-17 09:46:40 -0600

    v1.4.16 - search on Enter key in Factura and Paciente textboxes

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Media.Imaging;
 using Reficio.ViewModels;
 
@@ -15,5 +16,17 @@ public partial class MainWindow : Window
             Icon = new WindowIcon("avares://Reficio/Resources/Reficio.icns");
         }
         catch { }
+    }
+
+    private void SearchBox_KeyDown(object? sender, KeyEventArgs e)
+    {
+        if (e.Key != Key.Enter) return;
+        if (sender is not TextBox) return;
+        if (DataContext is not MainViewModel vm) return;
+
+        if (vm.SelectedTabIndex == 1)
+            vm.SearchFacturaCommand.Execute(null);
+        else if (vm.SelectedTabIndex == 2)
+            vm.SearchPacienteCommand.Execute(null);
     }
 }
